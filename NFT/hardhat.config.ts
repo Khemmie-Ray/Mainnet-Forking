bd495a703cad8d10f84f154628628eb85836b8a8
Khemmie-Ray <atokemmy@gmail.com>
2024-02-26 18:47:04 +0100

    initial commit

--- NFT/hardhat.config.ts
+++ NFT/hardhat.config.ts
@@ -1,26 +1,8 @@
-// import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-require("dotenv").config({ path: ".env" });
 
-const { ALCHEMY_MAINNET_API_KEY_URL, ALCHEMY_SEPOLIA_API_KEY_URL, ALCHEMY_MUMBAI_API_KEY_URL, ACCOUNT_PRIVATE_KEY} = process.env;
-
-module.exports = {
+const config: HardhatUserConfig = {
   solidity: "0.8.24",
-  networks: {
-    hardhat: {
-      forking: {
-        url: ALCHEMY_MAINNET_API_KEY_URL,
-      }
-    },
-    sepolia: {
-      url: ALCHEMY_SEPOLIA_API_KEY_URL,
-      accounts: [ACCOUNT_PRIVATE_KEY],
-    },
-    mumbai: {
-      url: ALCHEMY_MUMBAI_API_KEY_URL,
-      accounts: [ACCOUNT_PRIVATE_KEY],
-    }
-  },
-  lockGasLimit: 200000000000,
-  gasPrice: 10000000000,
 };
+
+export default config;
